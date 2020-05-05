b7625c8b8a34ffaf9dba974e2466d8fc740f0477
Mikael-R <mikarg9@gmail.com>
2020-05-05 13:58:49 -0300

    Transforma o conteúdo da url em arquivo json

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const Feed = require('rss-to-json')
+const fs = require('fs')
 const url = 'https://www.mixmods.com.br/feeds/posts/default?alt=rss'
 
+
+function dataAtual(){
+  var data = new Date();
+  var data_format = data.getDate() + '/' + (data.getMonth()+1) + '/' + data.getFullYear() + ' ' + data.getHours() + ':' + data.getMinutes();
+  return data_format;
+}
+
+
+function createJson(rss) { // 24 postagens
+    fs.writeFile('./posts.json', JSON.stringify(rss), {encoding: 'utf-8', flag: 'w'},
+    function (err) {
+        if (err) throw err
+        console.log(`[${dataAtual()}] Json atualizado`)
+    })
+}
+
+
 Feed.load(url, function(err, rss){
-    console.log(rss.items[0].title) // título
-    console.log(rss.items[0].description) // descrição
-    console.log(rss.items[0].url)  // url
-    console.log(rss.items[0].category._) // categoria
-    console.log(rss.items[0].pubDate) // data de publicação
+    createJson(rss)
+/*
+    console.log(rss.items[0].title)        // título
+    console.log(rss.items[0].description)  // descrição
+    console.log(rss.items[0].url)          // url
+    console.log(rss.items[0].category._)   // categoria
+    console.log(rss.items[0].pubDate)      // data de publicação
     console.log(rss.items[0].atom_updated) // data da última modificação
+*/
 })
