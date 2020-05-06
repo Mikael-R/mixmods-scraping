2e82502d94cb3789e8f2ad694a8fd145f3f5b56d
Mikael-R <mikarg9@gmail.com>
2020-05-06 13:23:15 -0300

    Código refatorado usando outra lib

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const feed = require('rss-to-json')
+const Parser = require('rss-parser')
+const parser = new Parser();
 
-feed.load('https://www.mixmods.com.br/feeds/posts/default?alt=rss', function(err, rss) {
-    let numero_posts = 4 // máximo 24
-    for (let n = 0; n < numero_posts; n++) {
-        console.log(`==========================================
-        [Mixmods-Notifier]
-${rss.items[n].title}
-${rss.items[n].url}
-  ● Categoria: ${rss.items[n].category._}
-  ● Publicado: ${rss.items[n].pubDate.substr(6,20)}
-  ● Última atualização: ${rss.items[n].atom_updated.substr(0,19).replace('T', ' ')}
-==========================================
-        `)}})
+(async () => {
+  const feed = await parser.parseURL('https://www.mixmods.com.br/feeds/posts/default?alt=rss')
+  console.log(`
+${feed.title}
+${feed.description}.
+  `)
+
+  for (let n = 0; n < 4; n++) { // máximo 25 posts(0 a 24)
+
+    let categorias = ''
+    for (let c in feed.items[n].categories) {
+      categorias += feed.items[n].categories[c]._ + ' |'
+    }
+
+    console.log(`============================================================================
+                          [Mixmods-Notifier]
+● Título: ${feed.items[n].title}
+● Link: ${feed.items[n].link}
+● Categorias: ${categorias}
+● Publicado: ${String(Number(feed.items[n].pubDate.substr(5,3)) - 1) + ' ' + feed.items[n].pubDate.substr(8,9)}
+============================================================================
+    `)
+  }
+
+})()
